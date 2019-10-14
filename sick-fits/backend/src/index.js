77c10aeb88e5b70de8293763c8a6b68dec1c8b4a
FMGordillo <facundomgordillo@gmail.com>
2019-10-14 14:23:36 -0300

    NOW it's working!

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require("dotenv").config();
+const createServer = require("./createServer");
+const db = require("./db");
+const server = createServer();
+
+// TODO: Use Express.js middleware to handle cookies
+// TODO: Use Express.js middleware to populate user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  sv => {
+    console.log(`Server is running on port: http://localhost:${sv.port}`);
+  }
+);
